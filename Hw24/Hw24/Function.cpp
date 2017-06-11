0d22c2e9fad9aedc6bf14e89aa892bcce3484b09
Rauf Jafarov <rauf_djafarov@inbox.ru>
2017-06-11 15:53:32 +0400

    last commit (alpha)

--- Hw24/Hw24/Function.cpp
+++ Hw24/Hw24/Function.cpp
@@ -9,10 +9,14 @@ char **Add(char **string, int &length);
 char **Remove(char **string, int &length, int num);
 int cmp(char *str1, char *str2);
 void Sort(char **string, int &length);
+char **Search(char **string, int length, int &counter2);
+char **Change(char **string, int length, int index);
+char **increaseStr(char **string, int &length);
 
 void Function(char **string, int &length)
 {
-	int count1 = 0, count2 = 0, key;
+	int count1 = 0, count2 = 0, key, save_key = 0, counter = 0;
+	char **search;
 	short s_x = 1;
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	while (true)
@@ -21,36 +25,47 @@ void Function(char **string, int &length)
 		system("cls");
 		for (int i = 0; i < 21; i++)
 		{
-			for (int j = 0; j < 31; j++)
+			for (int j = 0; j < 101; j++)
 			{
-				if (i % 2 == 0 && j > 0 && j < 30)
+				if (i % 2 == 0 && j > 0 && j < 30 || i == 0 && j > 55 && j < 100 || i == 4 && j > 55 && j < 100)
 					cout << char(205);
-				else if (j == 0 && i > 0 && i < 20|| j == 30 && i > 0 && i < 20)
+				else if (j == 0 && i > 0 && i < 20|| j == 30 && i > 0 && i < 20 || j == 55 && i > 0  && i < 4 || j == 100 && i > 0 && i < 4)
 					cout << char(186);
-				else if (i == 0 && j == 0)
+				else if (i == 0 && j == 0 || i == 0 && j == 55)
 					cout << char(201);
-				else if (i == 0 && j == 30)
+				else if (i == 0 && j == 30 || i == 0 && j == 100)
 					cout << char(187);
-				else if (i == 20 && j == 0)
+				else if (i == 20 && j == 0 || i == 4 && j == 55)
 					cout << char(200);
-				else if (i == 20 && j == 30)
+				else if (i == 20 && j == 30 || i == 4 && j == 100)
 					cout << char(188);
 				else
 					cout << " ";
 			}
 			cout << endl;
 		}
+		SetConsoleCursorPosition(h, { 56, 1 });
+		cout << " a - add, r - remove, f - find, c - change\n";
+		SetConsoleCursorPosition(h, { 56, 2 });
+		cout << " \t\tEsc - exit";
 		for (int i = 0 + count1; i < length; i++)
 		{
 			SetConsoleCursorPosition(h, { 1, x });
-			if (i < 10 + count1)
+			if (i < 10 + count1 && save_key != 102)
 			{
 				cout << string[i] << endl;
 				x += 2;
 			}
+			if (i < 10 + count1 && (save_key == 'f' || save_key == 'F') && i < counter)
+			{
+				cout << search[i] << endl;
+				x += 2;
+			}
 		}
 		SetConsoleCursorPosition(h, { 1, s_x });
-		key = getch();
+		if (kbhit)
+			key = getch();
+		Sleep(77);
 		if (key == 'a' || key == 'A')
 		{
 			SetConsoleCursorPosition(h, { 0, 21 });
@@ -61,10 +76,32 @@ void Function(char **string, int &length)
 			SetConsoleCursorPosition(h, { 0, 21 });
 			string = Remove(string, length, count2);
 		}
+		if (key == 'f' && length > 0)
+		{
+			SetConsoleCursorPosition(h, { 0, 21 });
+			search = Search(string, length, counter);
+			save_key = key;
+		}
 		if (key == 's' &&  length > count2 + 1 && s_x < 19)
 		{
 			s_x += 2;
 			count2++;
+		}
+		if (key == 'c' && save_key != 'f')
+		{
+			SetConsoleCursorPosition(h, { 0, 21 });
+			string = Change(string, length, count2);
+		}
+		if (key == 27)
+		{
+			SetConsoleCursorPosition(h, { 0, 21 });
+			if (save_key == 'f')
+			{
+				delete[]search;
+				save_key = 0;
+			}
+			else
+				return;
 		}
 		else if (count2 >= 9 && key == 's' && count2 + 1 < length)
 		{
@@ -120,8 +157,7 @@ char **Remove(char **string, int &length, int num)
 
 void Sort(char **string, int &length)
 {
-	long i, j;
-	int num1 = 0;
+	int i, j, num1 = 0;
 	char *x;
 	for (i = 0; i < length; i++)
 	{
@@ -185,4 +221,57 @@ int cmp(char *str1, char *str2)
 		}
 	}
 	return count;
+}
+
+char **Search(char **string, int length, int &counter2)
+{
+	int counter1 = 0, index = 0;
+	counter2 = 0;
+	char *search = new char[255];
+	char **searchStr = new char*[counter2];
+	cout << "Search: ";
+	cin.getline(search, 255);
+	while (search[index] != '\0')
+		index++;
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < 255; j++)
+		{
+			if (string[i][j] == search[counter1])
+				counter1++;
+			else if (counter1 < index)
+			{
+				counter1 = 0;
+				break;
+			}
+			if (counter1 == index)
+			{
+				searchStr = increaseStr(searchStr, counter2);
+				searchStr[counter2 - 1] = string[i];
+				break;
+			}
+		}
+		counter1 = 0;
+	}
+	return searchStr;
+}
+
+char **Change(char **string, int length, int index)
+{
+	delete[]string[index];
+	string[index] = new char[255];
+	cout << "Enter name and last name\n";
+	cin.getline(string[index], 255);
+	Sort(string, length);
+	return string;
+}
+
+char **increaseStr(char **string, int &length)
+{
+	char **tempStr = new char*[length + 1];
+	for (int i = 0; i < length; i++)
+		tempStr[i] = string[i];
+	length++;
+	delete[] string;
+	return tempStr;
 }
